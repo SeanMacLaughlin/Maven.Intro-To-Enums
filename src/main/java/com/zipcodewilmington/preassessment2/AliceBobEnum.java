4ebb57dd3c3784365680c0136f1f68f4456c2314
Sean MacLaughlin <seanmaclaughlin@zipcoders-MacBook-Pro.local>
2019-03-29 21:07:49 -0400

    Enums lab complete

---
+++ src/main/java/com/zipcodewilmington/preassessment2/AliceBobEnum.java
@@ -0,0 +1,24 @@
+package com.zipcodewilmington.preassessment2;
+
+public enum AliceBobEnum {
+    ALICE("Hey, my name is ALICE!"),
+    BOB("Hey, my name is BOB!");
+
+    private String catchPhrase;
+
+    AliceBobEnum(String catchPhrase){
+        this.catchPhrase = catchPhrase;
+    }
+
+    public String getCatchPhrase() {
+        return this.catchPhrase;
+    }
+
+    public boolean isAlice() {
+        return this.name().equals("ALICE");
+    }
+
+    public boolean isBob() {
+        return this.name().equals("BOB");
+    }
+}
